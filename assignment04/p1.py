a68706c55359d7587b21667f3f0f6866315758b3
Ahmed <abasimelahmed@gmail.com>
2025-09-24 11:33:13 +0200

    Initial commit

---
+++ assignment04/p1.py
@@ -0,0 +1,11 @@
+# CTMS-14
+# a4 p1.py
+# Ahmed Abasimel
+# aabasimel@constructor.university
+
+ch=input("Enter a character: ")
+n=int(input("Enter a number: "))
+
+for i in range(n):
+    print(ch ,end="")
+
