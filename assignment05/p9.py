619016c76e42fb8421537cf98bc9ac402496e13f
Ahmed <abasimelahmed@gmail.com>
2025-10-06 22:10:18 +0200

    Initial commit

---
+++ assignment05/p9.py
@@ -0,0 +1,12 @@
+text = input("Enter the main text: ")
+s = input("Enter the substring to be replaced: ")
+r = input("Enter the replacing substring: ")
+
+# Print the original text
+print(f"\nOriginal text: {text}")
+
+# Replace all occurrences of s with r
+new_text = text.replace(s, r)
+
+# Print the modified text
+print(f"After replacing: {new_text}")
