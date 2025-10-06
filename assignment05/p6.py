f3795b6fb74cae3d3995bc3b395578efab9a4166
Ahmed <abasimelahmed@gmail.com>
2025-10-06 17:45:24 +0200

    Initial commit

---
+++ assignment05/p6.py
@@ -0,0 +1,28 @@
+def count_vowels(s):
+    """
+    Count the number of vowels in a given string.
+    
+    Parameters:
+    s (str): The string to count vowels in
+    
+    Returns:
+    int: Number of vowels in the string
+    """
+    vowels = "aeiouAEIOU"
+    count = 0
+    
+    for char in s:
+        if char in vowels:
+            count += 1
+            
+    return count
+
+while True:
+    text = input("Enter a string (or press Enter to quit): ")
+    
+    if text == "":
+        print("Goodbye!")
+        break
+    
+    vowel_count = count_vowels(text)
+    print(f"Number of vowels: {vowel_count}\n")
