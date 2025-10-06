8b3a06e438cf81c077663c421b6ec05b7b2fb998
Ahmed <abasimelahmed@gmail.com>
2025-10-06 15:03:49 +0200

    Initial commit

---
+++ assignment05/p1.py
@@ -0,0 +1,12 @@
+# Function to convert gallons and cups to liters
+def to_liter(gallon, cup):
+    GALLON_TO_LITER = 3.7854
+    CUP_TO_LITER = 0.2366
+    return (gallon * GALLON_TO_LITER) + (cup * CUP_TO_LITER)
+
+cup = float(input("Enter volume in cups: "))
+gallon = float(input("Enter volume in gallons: "))
+
+total_liters = to_liter(gallon, cup)
+
+print("Total volume in liters:", total_liters)
---
+++ assignment05/p2.py
@@ -0,0 +1,5 @@
+"""Lenght of a sting usign built-in function"""
+
+s=input("Enter a string: ")
+length=len(s)
+print(f"The length of the sting is {length}")
