a33075228ccde9e95b507e45c7ddc760202c9799
Ahmed <abasimelahmed@gmail.com>
2025-10-06 21:42:38 +0200

    Initial commit

---
+++ assignment05/p8.py
@@ -0,0 +1,21 @@
+data = "Python is a great programming language"
+
+#This prints the list of words within the string
+words_list=data.split()
+print("a) List of words: ", words_list)
+
+
+
+
+
+uppercase_data=data.upper()
+print("b) Uppercase: ",uppercase_data)
+
+position=data.find("programming")
+print("c) Postion of 'programming': ", position)
+
+replaced_data=data.replace('g', 'G')
+print("d) After replacing 'g' with 'G': ", replaced_data)
+
+
+
