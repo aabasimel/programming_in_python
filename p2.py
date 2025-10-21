af938551ca5a10ce6e5a366f91ae5f2d2b200dc2
Ahmed <abasimelahmed@gmail.com>
2025-10-21 19:42:40 +0200

    Initial commit

---
+++ p2.py
@@ -0,0 +1,24 @@
+dict = {
+    "merry": "god",
+    "christmas": "jul",
+    "and": "och",
+    "happy": "gott",
+    "new": "nytt",
+    "year": "\u00E5r"  
+}
+
+def translate(list_en):
+    swedish_words=[]
+    for word in list_en:
+        translated=dict.get(word.lower(),word)
+        swedish_words.append(translated)
+    return swedish_words
+
+english_greeting = input("Enter your Christmas greeting: ")
+
+english_words=english_greeting.split()
+
+
+swedish_translation = translate(english_words)
+print("Swedish translation: ", " ".join(swedish_translation))
+
