2f7da7fe008462d11b9d40be32f8fb0cd8a7cd65
Ahmed <abasimelahmed@gmail.com>
2025-10-21 20:48:09 +0200

    Initial commit

---
+++ assignment07/p5.py
@@ -0,0 +1,16 @@
+# Collect integers from the user until a negative number is entered
+elements = []
+
+while True:
+    n = int(input("Enter an integer (negative to stop): "))
+    if n < 0:
+        break
+    elements.append(n)
+
+tup = tuple(elements)
+
+# Reverse the tuple
+reversed_tup = tup[::-1]
+
+# Print the reversed tuple
+print("Reversed tuple:", reversed_tup)
