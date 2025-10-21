e7c76e8178906f0422347dc58da4d461cc34dbc9
Ahmed <abasimelahmed@gmail.com>
2025-10-21 21:24:25 +0200

    Initial commit

---
+++ assignment07/p7.py
@@ -0,0 +1,37 @@
+
+stack=[]
+
+def push(num):
+    stack.append(num)
+    print(f'Pushing {num}')
+
+def pop():
+    if stack:
+        elem=stack.pop()
+        print(f'Popping element {elem}')
+    else:
+        print("Stack underflow")
+
+def empty():
+    print("Emptying stack")
+    while stack:
+        pop()
+while True:
+    command = input().strip().lower()
+    
+    if command == "s":  
+        num = int(input().strip())
+        push(num)
+    
+    elif command == "p":
+        pop()
+    
+    elif command == "e":
+        empty()
+    
+    elif command == "q":
+        print("Good Bye!")
+        break
+    
+    else:
+        print("Invalid command")
